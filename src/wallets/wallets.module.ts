0a601ee1bafdfdff32ee477c5811f875c8ed23dd
Yura195 <tereshin_95@mail.ru>
2022-04-11 17:49:38 +0700

    add wallet resolver

--- src/wallets/wallets.module.ts
+++ src/wallets/wallets.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { WalletEntity } from './entities/wallet.entity';
+import { WalletsResolver } from './wallets.resolver';
 import { WalletsService } from './wallets.service';
 
 @Module({
   controllers: [],
-  providers: [WalletsService],
+  providers: [WalletsService, WalletsResolver],
   imports: [TypeOrmModule.forFeature([WalletEntity])],
 })
 export class WalletsModule {}
